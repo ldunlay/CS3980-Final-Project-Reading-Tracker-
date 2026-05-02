7680c7f0fb337793aa2b4517131fdebb1c4f8a49
ldunlay <lindseydunlay@gmail.com>
2026-05-02 15:59:51 -0500

    added check to see if logs folder exists, if not, it creates one

--- logging_setup.py
+++ logging_setup.py
@@ -1,8 +1,11 @@
 from logging.handlers import TimedRotatingFileHandler
 from logging import StreamHandler, basicConfig, INFO
+import os
 
 
+# same code as professsor, added check to see if logs folder exists, create if it doesn't
 def setup_logging():
+    os.makedirs("./logs", exist_ok=True)
     console_log = StreamHandler()
     file_log = TimedRotatingFileHandler("./logs/app.log", when="d", interval=1)
     basicConfig(
