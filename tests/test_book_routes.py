e51091395ea253119dcf18b2e5dc976f30a3b43c
ldunlay <lindseydunlay@gmail.com>
2026-05-03 12:11:19 -0500

    added to testing of book routes

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -5,6 +5,9 @@ from models.models import (
     CurrentBook,
     UpNext,
     FinishedBook,
+    CurrentBookRequest,
+    UpNextRequest,
+    FinishedBookRequest,
 )
 
 # creation of book objects test
@@ -68,7 +71,7 @@ async def mock_finished_book() -> FinishedBook:
         publish_date="2020-01-01",
         startDate="2024-01-01",
         finishDate="2024-02-01",
-        rate=4,
+        rating=4,
         review="Great book!",
     )
 
@@ -114,3 +117,244 @@ async def test_get_finished_books(
 
     assert response.status_code == 200
     assert response.json()[0]["_id"] == str(mock_finished_book.id)
+
+
+# test of add new book requests
+
+
+@pytest.mark.anyio
+async def test_post_new_current_book(
+    default_client: AsyncClient, access_token: str
+) -> None:
+    payload = {
+        "title": "Test Book",
+        "author": "Test Author",
+        "num_pages": 300,
+        "genre": "Fiction",
+        "isbn": "1234567890",
+        "publish_date": "2020-01-01",
+        "startDate": "2024-01-01",
+        "current_page": 50,
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    response = await default_client.post(
+        "/api/current-books", json=payload, headers=headers
+    )
+
+    assert response.status_code == 201
+    assert response.json()["title"] == "Test Book"
+
+
+@pytest.mark.anyio
+async def test_post_new_finished_book(
+    default_client: AsyncClient, access_token: str
+) -> None:
+    payload = {
+        "title": "Test Book",
+        "author": "Test Author",
+        "num_pages": 300,
+        "genre": "Fiction",
+        "isbn": "1234567890",
+        "publish_date": "2020-01-01",
+        "startDate": "2024-01-01",
+        "finishDate": "2024-02-01",
+        "rating": 4,
+        "review": "Great book!",
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    response = await default_client.post(
+        "/api/finished-books", json=payload, headers=headers
+    )
+
+    assert response.status_code == 201
+    assert response.json()["title"] == "Test Book"
+
+
+@pytest.mark.anyio
+async def test_post_new_upnext_book(
+    default_client: AsyncClient, access_token: str
+) -> None:
+    payload = {
+        "title": "Test Book",
+        "author": "Test Author",
+        "num_pages": 300,
+        "genre": "Fiction",
+        "isbn": "1234567890",
+        "publish_date": "2020-01-01",
+        "added_date": "2024-01-01",
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    response = await default_client.post("/api/up-next", json=payload, headers=headers)
+
+    assert response.status_code == 201
+    assert response.json()["title"] == "Test Book"
+
+
+# test of count
+
+
+@pytest.mark.anyio
+async def test_get_current_books_count(
+    default_client: AsyncClient, mock_current_book: CurrentBook, access_token: str
+) -> None:
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = await default_client.get("/api/current-books", headers=headers)
+
+    books = response.json()
+
+    assert response.status_code == 200
+    assert len(books) == 1
+
+
+@pytest.mark.anyio
+async def test_get_finished_books_count(
+    default_client: AsyncClient, mock_finished_book: FinishedBook, access_token: str
+) -> None:
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = await default_client.get("/api/finished-books", headers=headers)
+
+    books = response.json()
+
+    assert response.status_code == 200
+    assert len(books) == 1
+
+
+@pytest.mark.anyio
+async def test_get_upnext_books_count(
+    default_client: AsyncClient, mock_upnext_book: UpNext, access_token: str
+) -> None:
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = await default_client.get("/api/up-next", headers=headers)
+
+    books = response.json()
+
+    assert response.status_code == 200
+    assert len(books) == 1
+
+
+# test of edit book requests
+
+
+@pytest.mark.anyio
+async def test_update_current_book(
+    default_client: AsyncClient, mock_current_book: CurrentBook, access_token: str
+) -> None:
+    test_payload = CurrentBookRequest(
+        title="Updated Test Book", author="Updated Test Author", startDate="2024-01-15"
+    )
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    url = f"/api/current-books/{str(mock_current_book.id)}"
+
+    response = await default_client.put(
+        url, content=test_payload.model_dump_json(), headers=headers
+    )
+
+    assert response.status_code == 200
+    assert response.json()["title"] == test_payload.title
+
+
+@pytest.mark.anyio
+async def test_update_finished_book(
+    default_client: AsyncClient, mock_finished_book: FinishedBook, access_token: str
+) -> None:
+    test_payload = FinishedBookRequest(
+        title="Updated Test Book", author="Updated Test Author", finishDate="2024-02-15"
+    )
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    url = f"/api/finished-books/{str(mock_finished_book.id)}"
+
+    response = await default_client.put(
+        url, content=test_payload.model_dump_json(), headers=headers
+    )
+
+    assert response.status_code == 200
+    assert response.json()["title"] == test_payload.title
+
+
+# test of delete requests for books
+
+
+@pytest.mark.anyio
+async def test_delete_current_book(
+    default_client: AsyncClient, mock_current_book: CurrentBook, access_token: str
+) -> None:
+    test_response = {"message": "Book deleted successfully."}
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    url = f"/api/current-books/{str(mock_current_book.id)}"
+
+    response = await default_client.delete(url, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
+
+
+@pytest.mark.anyio
+async def test_delete_finished_book(
+    default_client: AsyncClient, mock_finished_book: FinishedBook, access_token: str
+) -> None:
+    test_response = {"message": "Finished book deleted successfully."}
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    url = f"/api/finished-books/{str(mock_finished_book.id)}"
+
+    response = await default_client.delete(url, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
+
+
+@pytest.mark.anyio
+async def test_delete_upnext_book(
+    default_client: AsyncClient, mock_upnext_book: UpNext, access_token: str
+) -> None:
+    test_response = {"message": "Book deleted successfully."}
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    url = f"/api/up-next/{str(mock_upnext_book.id)}"
+
+    response = await default_client.delete(url, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
+
+
+# test of download for current books
+
+
+# test of upload for current books
